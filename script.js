1dcf78e123f754f6359c372c9a00c44d213d2310
weslen-paes <weslen.devweb@gmail.com>
2020-01-04 18:51:58 -0200

    criar a comidinha

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x : Math.floor(Math.random()* 15 + 1) * box,
+    y : Math.floor(Math.random()* 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "brown";
@@ -21,6 +25,12 @@ function criarCobrina(){
       context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
+    
 }
 
 document.addEventListener('keydown', update);
@@ -38,8 +48,10 @@ function iniciarJogo() {
     if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction =="down") snake[0].y = 0;
     if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+    
     criarBG();
-    criarCobrina(); 
+    criarCobrina();
+    drawFood(); 
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
